d9422b42c9afa113b1a13cbfe9519db372e4944d
Dan <zerafiall@me.com>
2022-01-29 21:05:27 -0700

    Added Bootstrap

--- src/login.php
+++ src/login.php
@@ -1,14 +1,16 @@
 <?php
-echo "Env: " . $_ENV['MYSQL_ROOT_PASSWORD'];
+include_once 'includes/dbc.php';
+echo $servername;
+echo $username;
+echo $password;
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
     <form action="includes/login-inc.php" method="post">
